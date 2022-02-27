169433d44437d1af05d2d31e571e61ad9a0d34c3
Юров Михаил Дмитриевич <mikhail.yurov@tattelecom.ru>
2022-02-27 16:17:32 +0300

    Initial Commit

---
+++ Otus_Homework_1/Otus_Homework_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Otus_Homework_1App.swift
+//  Otus_Homework_1
+//
+//  Created by developer on 27.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Otus_Homework_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
